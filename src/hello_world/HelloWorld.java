fa066e4b310be93c68cbac7e8873cb71b889aaf4
Daniel Zint <daniel.zint@mail.de>
2023-03-07 11:47:45 -0500

    Add javadoc commands to HelloWorld.java.

--- src/hello_world/HelloWorld.java
+++ src/hello_world/HelloWorld.java
@@ -1,6 +1,16 @@
 package hello_world;
 
+/**
+ * A program to output the message "Hello World".
+ * 
+ * @author Daniel Zint
+ * @version 0.1
+ */
 public class HelloWorld {
+
+    /**
+     * @param args arguments are not used in this program
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
